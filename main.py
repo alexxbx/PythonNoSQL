bd1e7618bf6bb3aa2ec7b2153714a21ab4afff70
Brendan <brendanbx@gmail.com>
2021-10-14 11:47:34 +0200

    Patch 1.0.0 Route fini

--- main.py
+++ main.py
@@ -1,62 +1,103 @@
 # pprint library is used to make the output look more pretty
 from flask import Flask
-from flask import request
+from flask import request, jsonify
+from flask_pymongo import PyMongo
+from bson.json_util import dumps
+from bson.objectid import ObjectId
 from flask import make_response
 import json
 from pymongo import MongoClient
 import random
+
+
+
 # connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
-
-client = MongoClient("mongodb+srv://admin:admin@animeproject.no3y7.mongodb.net/<AnimeProject?retryWrites=true&w=majority")
-db=client["project"]
-col = db["anime"]
-
-names = ['Naruto','One Piece']
-genre = ['Shonen', 'Seinen']
-
-for x in range(1, 501):
-    project = {
-        'name' : names[1],
-        'rating' : random.randint(1, 5),
-        'genre' : genre[0]
-   }
-
-x = col.find({}, {'anime_title': 'Chihayafuru'})
-for data in x:
-    print(data)
-#result=db.anime.insert_one(project)
-#print('Created an anime'.format(x,result.inserted_id))
-
-#Step 5: Tell us that you are done
-
-#print('finished creating :' + names[1])
-
-
 
 app = Flask(__name__)
 
+app.secret_key = "secret_key"
+
+#client = MongoClient("mongodb+srv://admin:admin@animeproject.no3y7.mongodb.net/<AnimeProject?retryWrites=true&w=majority")
+
+app.config['MONGO_URI'] = "mongodb+srv://admin:admin@animeproject.no3y7.mongodb.net/AnimeProject?retryWrites=true&w=majority"
+
+mongo = PyMongo(app)
+
+@app.route('/add', methods=['POST'])
+def add_anime():
+    _json = request.json
+    _name = _json['anime_title']
+    _episodes = _json['anime_num_episodes']
+
+    if _name and _episodes and request.method == 'POST':
+
+        id = mongo.db.anime.insert_one({'anime_title': _name, 'anime_num_episodes': _episodes})
+
+        resp = jsonify("Anime added successfully")
+
+        resp.status_code = 200
+
+        return resp
+
+    else:
+        return not_found()
 
 
-@app.route('/', methods=["GET", "POST"])
-def acceuil():
-    """la fonction ouvre le fichier JSON afin de récupérer ce qu'il se trouve"""
+@app.route('/project', methods=['GET'])
+def animes():
+    animes = mongo.db.anime.find()
+    resp = dumps(animes)
+    return resp
 
-    if request.method == "GET":
-        #id = request.args["id"]
-        with open('.json') as mon_fichier:
-            data = mon_fichier.read()
-        return make_response(data ,200)
+@app.route('/project/<id>')
+def anime(id):
+    anime = mongo.db.anime.find_one({'_id':ObjectId(id)})
+    resp = dumps(anime)
+    return resp
 
-    elif request.method == "POST":
-        id = request.args["id"]
-        with open(id+'.json', 'w') as mon_fichier:
-            json.dump(request.get_json(), mon_fichier)
-        return make_response("ok", 200)
+@app.route('/delete/<id>', methods=['DELETE'])
+def delete_anime(id):
+    mongo.db.anime.delete_one({'_id': ObjectId(id)})
+    resp = jsonify("Anime deleted successfully")
+
+    resp.status_code = 200
+
+    return resp
+
+@app.route('/update/<id>', methods=['PUT'])
+def update_anime(id):
+    _id = id
+    _json = request.json
+    _name = _json['anime_title']
+    _episodes = _json['anime_num_episodes']
+
+    if _name and _episodes and _id and request.method == 'PUT':
+        mongo.db.anime.update_one({'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {'anime_title': _name, 'anime_num_episodes': _episodes}})
+
+        resp = jsonify("Anime updated successfully")
+
+        resp.status_code = 200
+
+        return resp
+    else:
+        return not_found()
+
+@app.errorhandler(404)
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message':'Not Found :' + request.url
+    }
+    resp = jsonify(message)
+
+    resp.status_code = 404
+
+    return resp
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
-if (__name__) == '__main__':
-    app.run(
-        host="10.138.33.247",
-        port=8081,
-        debug=True,
-    )
+
+
+
